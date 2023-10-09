d0ee3eb9b660bca104010c60ae955ef74c69b9c1
XavierSantos <38563643+SoulsLover@users.noreply.github.com>
2023-10-09 15:51:43 +0100

    Add files via upload

---
+++ Object Repository/Page_Resultados de pesquisa para o termo qu_4d046e/span_Carrinho_2.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Carrinho_2</name>
+   <tag></tag>
+   <elementGuidId>a1291198-5737-437d-8f4c-ab9eb7f21eae</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//div[@id='product-search-results']/div/div[2]/div[2]/div/div/div/div/div[2]/div[2]/div[3]/div[2]/button/span[2])[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
