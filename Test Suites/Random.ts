66484bacc6eb24d61843d37c7f01ea689c826905
xavier.a.santos <xavier.a.santos@MININT-CAEUVD4.noesis.pt>
2023-10-17 17:10:25 +0100

    adjustments

---
+++ Test Suites/Random.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Random</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>49c95dac-816a-4b39-ac71-0a9abf9c512b</testSuiteGuid>
+</TestSuiteEntity>
